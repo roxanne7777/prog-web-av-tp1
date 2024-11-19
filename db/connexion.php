fa3209efaa841af1d3a82ffb9d2b46dd3e23ee01
roxanne7777 <0533864@cmontmorency.qc.ca>
2024-11-18 22:49:50 -0500

    ajouter webdev

--- db/connexion.php
+++ db/connexion.php
@@ -1,9 +1,9 @@
 <?php
 try {
     $dbhost = 'localhost';
-    $dbname = 'systeme';
-    $dbuser = 'root';
-    $dbpass = '';
+    $dbname = 'e2495441';
+    $dbuser = 'e2495441';
+    $dbpass = 'CFtnBPaxnO7mz8yGxI1Z';
     $dbport = 3306;
     
     $pdo = new PDO("mysql:host=$dbhost; dbname=$dbname; port=$dbport; charset=utf8", $dbuser, $dbpass);
